41f114d9cddb2590a988151aeb33d8e78f5b62ef
Caleb Calderwood <calderwoodc@gmail.com>
2024-04-12 16:19:16 -0600

    header and footer load on index.html

--- src/app.jsx
+++ src/app.jsx
@@ -17,10 +17,39 @@
 //     </div>
 //   );
 // }
+
+
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
 
 export default function App() {
-  return <div className='body bg-dark text-light'>App will display here</div>;
-}
+  return (
+    <div className="body bg-white text-dark">
+      <header className="header bg-secondary text-light sticky-top">
+        <h1>Smart Workouts</h1>
+  
+        <nav>
+          <menu>
+            <li className="menu nav-item"><a className="nav-link active" href="index.html">Home</a></li>
+            <li className="menu nav-item"><a className="nav-link" href="user_workouts.html">Workouts</a></li>
+            <li className="menu nav-item"><a className="nav-link" href="workout_catalog.html">Workout Catalog</a></li>
+            <li className="menu nav-item"><a className="nav-link" href="account.html">Account</a></li>
+          </menu>
+        </nav>
+  
+        <hr />
+      </header>
+
+
+      <main>
+        Main things
+      </main>
+
+      <footer className="footer bg-secondary text-light">
+        <a href="about.html" style={{ display: "inline-block" }}>About</a>
+        <a href="https://github.com/Spitfyre4/startup" style={{ float: "right" }}>GitHub</a>
+      </footer>
+    </div>
+  );
+    }
